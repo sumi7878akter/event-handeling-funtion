11826ef7042c90087e73634ecd8e890c77be1bf8
sumi7878akter <jannatulislamsumi0@gmail.com>
2024-03-30 11:59:33 +0600

    all done

---
+++ script.js
@@ -0,0 +1,48 @@
+// Declaring a array
+{
+    const atiya=['atia,sakila,mim'];
+    console.log(atiya)
+}
+// declaring a object
+{
+    const mim={
+        name:"atia",
+        age:19,
+        group:"Web Development",
+        location:"singair"
+
+    }
+    console.log(mim)
+}
+// javascript array push
+{
+    const atia=['atia,sakila,mim'];
+    atia.push('sumi')
+    console.log(atia)  
+}
+// array pop
+{
+    var atia=['atia,sakila,mim,sumi'];
+    atia.pop("sumi");
+    console.log(atia)  
+}
+// length
+
+{
+    const atiaa=['atia,sakila,mim,sumi'];
+    console.log(atiaa.length)
+}
+// if,else
+var code=prompt ("Want to get  your giftbox? Select a  code-atiya/sakila")
+
+
+
+code1="atiya";
+code2="sakila";
+Giftbox="Congratulations!You have won the gift box!";
+sorry="We are extremely sorry,you have select the wrong code";
+if(code=code1)
+
+console.log(Giftbox);
+else ();
+console.log(sorry);
